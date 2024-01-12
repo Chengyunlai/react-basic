31c300907d54dd4fa6602c601a53ca7d10465515
cyrus <19550211498@163.com>
2024-01-12 20:51:41 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 const students = [
   {id:1,name:'chengyunlai'},
   {id:2,name:'zhangsan'},
@@ -14,7 +15,7 @@ function Button(){
 
 const flag = false
 
-const user_role = 'vip'
+var user_role = 'vip'
 
 function is_vip(){
   if (user_role  === 'vip')
@@ -23,11 +24,30 @@ function is_vip(){
     return <p>hi is normal</p>
 }
 
+
 function handleClick(flag,e){
   console.log(flag,e)
+  // 无效，改变不了函数的判断
+  user_role = 'not vip'
 }
 
 function App() {
+  const [state_vip,modify_state_vip] = useState('vip')
+
+  function data_view(){
+    if (state_vip  === 'vip')
+      return <p>wow is vip</p>
+    else
+      return <p>no is normal</p>
+  }
+
+  function change_data(){
+    if (state_vip  === 'vip')
+      modify_state_vip('not vip')
+    else
+      modify_state_vip('vip')
+  }
+
   return (
     <div className="App">
       { flag && "hello"}
@@ -37,6 +57,8 @@ function App() {
       </ul>
       {is_vip()}
       <button onClick={(e)=>{handleClick(flag,e)}}>Click</button>
+      {data_view()}
+      <button onClick={change_data}>点击我改变值</button>
     </div>
   );
 }
