c3b913cc5ccc10bf370a009e5b6354f03e5360ed
cyrus <19550211498@163.com>
2024-01-12 20:25:55 +0800

    组件与列表渲染

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+const students = [
+  {id:1,name:'chengyunlai'},
+  {id:2,name:'zhangsan'},
+  {id:3,name:'xiaoli'},
+]
+
+function Button(){
+  return(
+    <button>Click</button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      {"hello"}
+      <ul>
+        {students.map(item =><item key={item.id}>{item.name}<Button></Button></item>)}
+      </ul>
     </div>
   );
 }
